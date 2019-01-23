6b17091740dbd88b62a8b260cf3df82b360800a4
WDever <alsrbtkfkd7@gmail.com>
2019-01-23 19:29:46 +0900

    api fix

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,14 +1,14 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
-const top = `http://www.hansei-melon.com:8000/api/top-100/`;
-const check = `http://www.hansei-melon.com:8000/api/apply-check/`;
-const playlist = `http://www.hansei-melon.com:8000/api/music-list/`;
+const top = `https://www.hansei-melon.com:8000/api/top-100/`;
+const check = `https://www.hansei-melon.com:8000/api/apply-check/`;
+const playlist = `https://www.hansei-melon.com:8000/api/music-list/`;
 
 export const getTOP = () => axios.get(top);
 
 export const postAPPLY = (title, album, artist) => {
-  const apply = `http://www.hansei-melon.com:8000/api/apply/`;
+  const apply = `https://www.hansei-melon.com:8000/api/apply/`;
   const headers = {
     'Authorization': 'Token 1db7a12af9fdd4a6222c0d020f7ad458ccbfe3b8',
     'Content-Type': 'application/json'
@@ -26,16 +26,16 @@ export const getCHECK = () => axios.get(check);
 export const getPLAYLIST = () => axios.get(playlist);
 
 export const getALSearch = input => {
-  const search = `http://www.hansei-melon.com:8000/api/get_music_album/?q=${input}`;
+  const search = `https://www.hansei-melon.com:8000/api/get_music_album/?q=${input}`;
   return axios.get(search);
 }
 
 export const getTSearch = input => {
-  const search = `http://www.hansei-melon.com:8000/api/get_music_title/?q=${input}`;
+  const search = `https://www.hansei-melon.com:8000/api/get_music_title/?q=${input}`;
   return axios.get(search);
 }
 
 export const getARSearch = input => {
-  const search = `http://www.hansei-melon.com:8000/api/get_music_artist/?q=${input}`;
+  const search = `https://www.hansei-melon.com:8000/api/get_music_artist/?q=${input}`;
   return axios.get(search);
 }
