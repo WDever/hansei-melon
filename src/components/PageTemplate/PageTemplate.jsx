b4d22b6ab5690e62863a5af6feeb4532fd1d0584
WDever <alsrbtkfkd7@gmail.com>
2019-04-09 23:03:21 +0900

    add footer

--- src/components/PageTemplate/PageTemplate.jsx
+++ src/components/PageTemplate/PageTemplate.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 import styles from './PageTemplate.scss';
 
 const cx = classNames.bind(styles);
@@ -8,11 +9,13 @@ const cx = classNames.bind(styles);
 const PageTemplate = ({ list, search }) => (
   <div className={cx('page-template')}>
     <div className={cx('search-wrapper')}>{search}</div>
-    <section className={cx('list-wrapper')}>
-      {list}
-    </section>
+    <section className={cx('list-wrapper')}>{list}</section>
+    <footer>
+      <Link to='/terms' className={cx('footer')}>이용 약관</Link>
+      <Link to='/privacy' className={cx('footer')}>개인정보처리방침</Link>
+    </footer>
   </div>
-  );
+);
 
 PageTemplate.propTypes = {
   list: PropTypes.element,
